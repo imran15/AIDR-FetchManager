94c1f2d3c9f1644b2f17d546b5f498c547134222
kirkh <alex.mikhashchuk@gmail.com>
2013-08-13 15:03:52 +0300

    Small fix.

--- src/main/webapp/resources/js/aidrfm/collection-details/controller/CollectionDetailsController.js
+++ src/main/webapp/resources/js/aidrfm/collection-details/controller/CollectionDetailsController.js
@@ -457,6 +457,7 @@ Ext.define('AIDRFM.collection-details.controller.CollectionDetailsController', {
                 'Accept': 'application/json'
             },
             success: function (response) {
+                var resp = Ext.decode(response.responseText);
                 if (resp.data && resp.data.length > 0) {
                     var count = resp.data.length;
                     if (count > 0) {
