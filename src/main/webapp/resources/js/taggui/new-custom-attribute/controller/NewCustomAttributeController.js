8dd69a36eea60bc6372975e3740cd8e0afcc2b2d
kirkh <alex.mikhashchuk@gmail.com>
2013-09-11 13:48:51 +0300

    'New Custom Attribute' screen client side (without save and validation).

--- src/main/webapp/resources/js/taggui/new-custom-attribute/controller/NewCustomAttributeController.js
+++ src/main/webapp/resources/js/taggui/new-custom-attribute/controller/NewCustomAttributeController.js
@@ -11,6 +11,58 @@ Ext.define('TAGGUI.new-custom-attribute.controller.NewCustomAttributeController'
 
             'new-custom-attribute-view': {
                 beforerender: this.beforeRenderView
+            },
+
+            "#nameInfo": {
+                render: function (infoPanel, eOpts) {
+                    var tip = Ext.create('Ext.tip.ToolTip', {
+                        trackMouse: true,
+                        html: 'Give a name to your attribute. For example, Violence, Region.',
+                        target: infoPanel.el,
+                        dismissDelay: 0
+                    });
+                }
+            },
+
+            "#codeInfo": {
+                render: function (infoPanel, eOpts) {
+                    var tip = Ext.create('Ext.tip.ToolTip', {
+                        trackMouse: true,
+                        html: 'Attribute code consists of alpha-numeric short code name to a attribute. ' +
+                            'Spaces are not allowed in the code name. For example, missing_person, syria_region are valid code names',
+                        target: infoPanel.el,
+                        dismissDelay: 0
+                    });
+                }
+            },
+
+            "#descriptionInfo": {
+                render: function (infoPanel, eOpts) {
+                    var tip = Ext.create('Ext.tip.ToolTip', {
+                        trackMouse: true,
+                        html: 'Give a description to your attribute. For example, Geographic regions centered on Syria, Report of violence.',
+                        target: infoPanel.el,
+                        dismissDelay: 0
+                    });
+                }
+            },
+
+            "#attributeCancelCreate": {
+                click: function (btn, e, eOpts) {
+                    document.location.href = BASE_URL + '/protected/' + CRISIS_CODE + '/predict-new-attribute';
+                }
+            },
+
+            "#attributeCreate": {
+                click: function (btn, e, eOpts) {
+                    this.isExist();
+                }
+            },
+
+            "#addLabel": {
+                click: function (btn, e, eOpts) {
+                    this.addLabel();
+                }
             }
 
         });
@@ -24,6 +76,108 @@ Ext.define('TAGGUI.new-custom-attribute.controller.NewCustomAttributeController'
         taggerCollectionDetailsController = this;
 
         var me = this;
+    },
+
+    addLabel: function() {
+        var me = this;
+
+        var labels = me.mainComponent.labelsStore.getRange();
+
+//      TODO validation. If fields empty of if attribute with code already exist
+        var r = {};
+        r.name = me.mainComponent.nameLabelE.getValue();
+        r.description = me.mainComponent.DescriptionLabelE.getValue();
+        r.code = me.mainComponent.codeLabelE.getValue();
+        me.clearAttributeFields();
+
+        labels.push(r);
+
+        me.mainComponent.labelsStore.loadData(labels, true);
+    },
+
+    clearAttributeFields: function(){
+        var me = this;
+        me.mainComponent.nameLabelE.setValue();
+        me.mainComponent.DescriptionLabelE.setValue();
+        me.mainComponent.codeLabelE.setValue();
+        me.mainComponent.nameLabelE.clearInvalid();
+        me.mainComponent.DescriptionLabelE.clearInvalid();
+        me.mainComponent.codeLabelE.clearInvalid();
+    },
+
+    isExist: function () {
+        AIDRFMFunctions.setAlert("Ok", "Will be implemented soon.");
+
+        var me = this;
+
+//        var form = Ext.getCmp('collectionForm').getForm();
+//        var code = form.findField('code');
+//        Ext.Ajax.request({
+//            url: 'collection/exist.action',
+//            method: 'GET',
+//            params: {
+//                code: code.getValue()
+//            },
+//            headers: {
+//                'Accept': 'application/json'
+//            },
+//            success: function (response) {
+//                var response = Ext.decode(response.responseText);
+//                if (response.data) {
+//                    AIDRFMFunctions.setAlert('Error', 'Collection Code already exist. Please select another code');
+//                    code.markInvalid("Collection Code already exist. Please select another code");
+//                } else {
+//                    me.saveAttribute();
+//                }
+//            }
+//        });
+    },
+
+    saveAttribute: function () {
+        var me = this;
+
+//        var mask = AIDRFMFunctions.getMask(true, 'Saving collection ...');
+//        mask.show();
+//
+//        if (AIDRFMFunctions.mandatoryFieldsEntered()) {
+//            var form = Ext.getCmp('collectionForm').getForm();
+//            Ext.Ajax.request({
+//                url: 'collection/save.action',
+//                method: 'POST',
+//                params: {
+//                    name: form.findField('name').getValue(),
+//                    code: form.findField('code').getValue(),
+//                    track: form.findField('track').getValue(),
+//                    follow: form.findField('follow').getValue(),
+//                    geo: form.findField('geo').getValue(),
+//                    langFilters: form.findField('langFilters').getValue()
+//                },
+//                headers: {
+//                    'Accept': 'application/json'
+//                },
+//                success: function (response) {
+//                    AIDRFMFunctions.setAlert("Ok", ["Collection created successfully.", "You will be redirected to Home screen."]);
+//                    mask.hide();
+//
+//                    var maskRedirect = AIDRFMFunctions.getMask(true, 'Redirecting ...');
+//                    maskRedirect.show();
+//
+////                    wait for 3 sec to let user read information box
+//                    var isFirstRun = true;
+//                    Ext.TaskManager.start({
+//                        run: function () {
+//                            if (!isFirstRun) {
+//                                document.location.href = BASE_URL + '/protected/home';
+//                            }
+//                            isFirstRun = false;
+//                        },
+//                        interval: 3 * 1000
+//                    });
+//                }
+//            });
+//        } else {
+//            mask.hide();
+//        }
     }
 
 });
--- src/main/webapp/resources/js/taggui/new-custom-attribute/view/NewCustomAttributePanel.js
+++ src/main/webapp/resources/js/taggui/new-custom-attribute/view/NewCustomAttributePanel.js
@@ -33,18 +33,247 @@ Ext.define('TAGGUI.new-custom-attribute.view.NewCustomAttributePanel', {
             flex: 1
         });
 
-//        this.taggerDescription = Ext.create('Ext.form.Label', {
-//            cls: 'styled-text',
-//            margin: '0 0 15 0',
-//            text: 'Details for attribute ',
-//            flex: 1
-//        });
-
         this.horisontalLine = Ext.create('Ext.container.Container', {
             width: '100%',
             html: '<div class="horisontalLine"></div>'
         });
 
+        this.codeE = Ext.create('Ext.form.field.Text', {
+            fieldLabel: 'Code',
+            name: 'code',
+            allowBlank: false,
+            flex: 1,
+            emptyText: 'Enter code for Attribute',
+            maxLength: 15,
+            maxLengthText: 'The maximum length for this field is 15 ',
+            maskRe: /[^ ]/
+        });
+
+        this.nameE = Ext.create('Ext.form.field.Text', {
+            flex: 1,
+            fieldLabel: 'Name',
+            name: 'name',
+            allowBlank: false,
+            emptyText: 'Enter name for Attribute'
+        });
+
+        this.DescriptionE = Ext.create('Ext.form.field.Text', {
+            fieldLabel: 'Description',
+            name: 'description',
+            allowBlank: true,
+            flex: 1,
+            emptyText: 'Enter description for Attribute'
+        });
+
+        this.codeLabelE = Ext.create('Ext.form.field.Text', {
+            fieldLabel: 'Code',
+            name: 'code',
+            allowBlank: false,
+            flex: 1,
+            emptyText: 'Enter code for Label',
+            maxLength: 15,
+            maxLengthText: 'The maximum length for this field is 15 ',
+            maskRe: /[^ ]/
+        });
+
+        this.nameLabelE = Ext.create('Ext.form.field.Text', {
+            flex: 1,
+            fieldLabel: 'Name',
+            name: 'name',
+            allowBlank: false,
+            emptyText: 'Enter name for Label'
+        });
+
+        this.DescriptionLabelE = Ext.create('Ext.form.field.Text', {
+            fieldLabel: 'Description',
+            name: 'description',
+            allowBlank: true,
+            flex: 1,
+            emptyText: 'Enter description for Label'
+        });
+
+        this.newLabelL = Ext.create('Ext.form.Label', {
+            flex: 1,
+            text: 'New Label',
+            padding: '15 0 0 0',
+            cls: 'header-h2'
+        });
+
+        this.saveButton = Ext.create('Ext.Button', {
+            text: 'Save',
+            margin: '0 10 0 10',
+            cls: 'btn btn-green',
+            id: 'attributeCreate'
+        });
+
+        this.cancelButton = Ext.create('Ext.Button', {
+            text: 'Cancel',
+            cls: 'btn btn-red',
+            id: 'attributeCancelCreate'
+        });
+
+        this.addLabelButton = Ext.create('Ext.Button', {
+            text: 'Add Label',
+            cls: 'btn btn-green',
+            id: 'addLabel'
+        });
+
+        this.labelsStore = Ext.create('Ext.data.JsonStore', {
+            pageSize: 100,
+            storeId: 'labelsStore',
+            fields: ['code', 'description', 'name'],
+            proxy: {
+                type: 'ajax',
+                url: '',
+                reader: {
+                    root: 'data',
+                    totalProperty: 'total'
+                }
+            },
+            autoLoad: false
+        });
+
+        this.labelsTpl = new Ext.XTemplate(
+            '<div class="attribute-list">',
+
+            '<tpl for=".">',
+
+            '<tpl if="xindex == 1">' +
+                '<div class="attributes-title"><div class="inner"><h2>Labels</h2></div></div>' +
+            '</tpl>' +
+
+            '<div class="attribute-item">',
+
+            '<div class="content">',
+
+            '<div class="img">',
+            '<a href="' + BASE_URL + '/protected/{nominalAttributeID}/attribute-details"><img alt="Attribute image" src="/AIDRFetchManager/resources/img/AIDR/AIDR_EMBLEM_CMYK_COLOUR_HR.jpg" width="70"></a>',
+            '</div>',
+
+            '<div class="info">',
+            '<div class="styled-text-14" id="docCountField_{id}">Name:&nbsp;&nbsp;&nbsp;{name}</div>',
+            '<div class="styled-text-14" id="docCountField_{id}">Code:&nbsp;&nbsp;&nbsp;{code}</div>',
+            '<div class="styled-text-14" id="docCountField_{id}">Description:&nbsp;&nbsp;&nbsp;{description}</div>',
+            '</div>',
+
+            '</div>',
+            '</div>',
+
+            '</tpl>',
+
+            '</div>'
+        );
+
+        this.labelsView = Ext.create('Ext.view.View', {
+            store: this.labelsStore,
+            id: 'labelsViewId',
+            tpl: this.labelsTpl,
+            itemSelector: 'li.crisesItem'
+        });
+
+        this.editForm = Ext.create('Ext.form.Panel', {
+            id: 'collectionForm',
+            bodyCls: 'no-border',
+            items: [
+                {
+                    xtype: 'container',
+                    defaults: {
+                        width: 350,
+                        labelWidth: 120
+                    },
+                    layout: {
+                        type: 'vbox',
+                        align: 'stretch'
+                    },
+                    items: [
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            margin: '5 0',
+                            items: [
+                                this.codeE,
+                                {
+                                    border: false,
+                                    bodyStyle: 'background:none',
+                                    html: '<img src="/AIDRFetchManager/resources/img/info.png"/>',
+                                    height: 22,
+                                    width: 22,
+                                    id: 'codeInfo'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            margin: '5 0',
+                            items: [
+                                this.nameE,
+                                {
+                                    border: false,
+                                    bodyStyle: 'background:none',
+                                    html: '<img src="/AIDRFetchManager/resources/img/info.png"/>',
+                                    height: 22,
+                                    width: 22,
+                                    id: 'nameInfo'
+                                }
+                            ]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            margin: '5 0 0 0',
+                            padding: '0 0 20 0',
+                            cls: 'bordered-bottom',
+                            items: [
+                                this.DescriptionE,
+                                {
+                                    border: false,
+                                    bodyStyle: 'background:none',
+                                    html: '<img src="/AIDRFetchManager/resources/img/info.png"/>',
+                                    height: 22,
+                                    width: 22,
+                                    id: 'descriptionInfo'
+                                }
+                            ]
+                        },
+                        this.newLabelL,
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            margin: '5 0',
+                            items: [
+                                this.codeLabelE
+                            ]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            margin: '5 0',
+                            items: [
+                                this.nameLabelE
+                            ]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            margin: '5 0 0 0',
+                            items: [
+                                this.DescriptionLabelE
+                            ]
+                        },
+                        {
+                            xtype: 'container',
+                            layout: 'hbox',
+                            padding: '15 0 15 0',
+                            cls: 'bordered-bottom',
+                            items: [
+                                this.addLabelButton
+                            ]
+                        }
+                    ]
+                }
+            ]
+        });
 
         this.items = [
             this.breadcrumbs,
@@ -61,14 +290,18 @@ Ext.define('TAGGUI.new-custom-attribute.view.NewCustomAttributePanel', {
                 },
                 items: [
                     this.taggerTitle
-//                    ,
-//                    this.taggerDescription
                 ]
             },
+            this.editForm,
+            this.labelsView,
             {
                 xtype: 'container',
-                width: '100%',
-                html: '<div class="horisontalLine"></div>'
+                layout: 'hbox',
+                margin: '15 0 0 0',
+                items: [
+                    this.saveButton,
+                    this.cancelButton
+                ]
             }
         ];
 
