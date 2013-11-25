ebdfe847b25f4ba9d087fe66a0260c445ce31e53
jlucas <jikimlucas@me.com>
2013-11-25 16:05:06 +0300

    updated footer/header

--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -36,7 +36,7 @@
     </div>
 </div>
 
-<div class="site-footer">
+<div class="site-footer" style="position:absolute;bottom: 0;">
     <div class="footer">
         <a style="text-decoration: none;color: #ffffff" href="http://www.qcri.qa/">A project by <img align="middle" id="footerqcrilogo" src="${pageContext.request.contextPath}/resources/img/qcri-gray-horiz.png"></a>
     </div>
--- src/main/webapp/resources/js/taggui/new-custom-attribute/Application.js
+++ src/main/webapp/resources/js/taggui/new-custom-attribute/Application.js
@@ -29,7 +29,7 @@ Ext.application({
                             xtype: 'container',
                             width: '100%',
                             html: '<div class="headerWrapper">' +
-                                '<div id="header" class="header"></div>' +
+                                '<div id="header" class="header"><img class="headeraidrlogo" src="' + BASE_URL + '/resources/img/AIDR/aidr_logo_240x90.png"></div>' +
                                 '</div>'
                         },
                         {
@@ -38,7 +38,7 @@ Ext.application({
                         {
                             xtype: 'container',
                             width: '100%',
-                            html: '<div class="site-footer">' +
+                            html: '<div class="site-footer"><div class="footer"><a style="text-decoration: none;color: #ffffff" href="http://www.qcri.qa/">A project by <img align="middle" id="footerqcrilogo" src="' + BASE_URL + '/resources/img/qcri-gray-horiz.png"></a></div>' +
                                 '</div>'
                         }
                     ]
